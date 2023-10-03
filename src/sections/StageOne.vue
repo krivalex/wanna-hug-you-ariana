5c2bfbd72f4d6e361794bdf5f2f106eaa6d6e5de
krivalex <new.krivalex@gmail.com>
2023-10-03 23:22:31 +0600

    progress

--- src/sections/StageOne.vue
+++ src/sections/StageOne.vue
@@ -3,19 +3,90 @@
 <!-- стихотворение про море и девушку - красиво выходит -->
 <!-- в конце кнопку перехода на следующий стейдж -->
 <template>
-  <div class="slow-text text">
-    <h1>Одень наушники и смотри со звуком</h1>
+  <div class="background">
+    <video autoplay loop>
+      <source src="../assets/bg-video-stocks/rain.mp4" type="video/mp4" />
+    </video>
+    <audio autoplay loop>
+      <source src="../assets/bg-audio/Wet_Hands.mp3" type="audio/mp3" id="audio" />
+    </audio>
   </div>
-  <div class="slow-text text">
-    <h1>Wanna hug you, Ariana</h1>
+  <div class="wheel-slow-text">
+    <div v-if="firstText" class="slow-text text">
+      <h1>Одень наушники и смотри со звуком</h1>
+    </div>
+    <div v-if="secondText" class="slow-text text">
+      <h1>Wanna hug you, Ariana</h1>
+    </div>
+    <p-button v-if="nextStageApprove" label="Я готова" @click="nextStage" class="next-stage-button p-button-rounded" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PButton from 'primevue/button'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
-<style lang="scss">
-.slow-text {
-  animation: slow-text 6s linear infinite;
+const router = useRouter()
+const nextStageApprove = ref(false)
+const firstText = ref(false)
+const secondText = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    firstText.value = true
+  }, 2000)
+  setTimeout(() => {
+    secondText.value = true
+  }, 6000)
+  setTimeout(() => {
+    nextStageApprove.value = true
+  }, 10000)
+})
+
+const nextStage = () => {
+  router.push('/stageTwo')
+}
+</script>
+
+<style scoped lang="scss">
+.next-stage-button {
+  padding: 0.5rem 2rem;
+  font-size: 1.5rem;
+  animation: slow-text 12s linear infinite;
+}
+
+.wheel-slow-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 100%;
+
+  .text {
+    font-size: 1rem;
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+    text-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    margin: 2rem 10px;
+  }
+
+  .slow-text {
+    animation: slow-text 6s linear infinite;
+  }
+}
+
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  min-width: 100%;
+  min-height: 100%;
+  overflow: hidden;
+  object-fit: contain;
 }
 
 @keyframes slow-text {
--- src/sections/StageTwo.vue
+++ src/sections/StageTwo.vue
@@ -1,4 +1,113 @@
 <!-- карусель с нейросетью и музыкой из ее фоток -->
-<template></template>
+<template>
+  <div class="content">
+    <PCarousel :circular="true" :value="videos">
+      <template #item="{ data }">
+        <div class="carousel-item" v-if="data">
+          <p-button v-if="!data.play" icon="pi pi-play" style="margin: 30px 0px; padding: 0.75rem 0; border-radius: 50%" @click.stop="playVideo(data.id)" />
+          <p-button v-else icon="pi pi-pause" class="button p-button-rounded" @click.stop="playStop(data.id)" />
+          <p>{{ data.title }}</p>
+          <video :id="data.title" class="image-carousel" :src="data.material" :alt="data.title" autoplay loop />
+        </div>
+      </template>
+    </PCarousel>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PCarousel from 'primevue/carousel'
+import PButton from 'primevue/button'
+import { ref } from 'vue'
+
+function playVideo(id: number) {
+  const video = videos.value.find((video) => video.id === id)
+  if (video) {
+    video.play = true
+    const videoElement = document.getElementById(video.title) as HTMLVideoElement
+    videoElement.play()
+  }
+  videos.value.forEach((video) => {
+    if (video.id !== id) {
+      video.play = false
+      const videoElement = document.getElementById(video.title) as HTMLVideoElement
+      videoElement.pause()
+    }
+  })
+}
+
+function playStop(id: number) {
+  const video = videos.value.find((video) => video.id === id)
+  if (video) {
+    video.play = false
+    const videoElement = document.getElementById(video.title) as HTMLVideoElement
+    videoElement.pause()
+  }
+  videos.value.forEach((video) => {
+    if (video.id !== id) {
+      video.play = false
+      const videoElement = document.getElementById(video.title) as HTMLVideoElement
+      videoElement.pause()
+    }
+  })
+}
+
+const videos = ref([
+  {
+    id: 1,
+    preview: 'src/assets/she/image/1.jpg',
+    material: 'src/assets/she/video/1.mp4',
+    title: 'Ты прекрасна как осень',
+    play: false,
+  },
+  {
+    id: 2,
+    preview: 'src/assets/she/image/2.jpg',
+    material: 'src/assets/she/video/2.mp4',
+    title: 'У тебя правда красивые глаза',
+    play: false,
+  },
+  {
+    id: 3,
+    preview: 'src/assets/she/image/3.jpg',
+    material: 'src/assets/she/video/3.mp4',
+    title: 'Если ты море, то я утонул',
+    play: false,
+  },
+  {
+    id: 4,
+    preview: 'src/assets/she/image/4.jpg',
+    material: 'src/assets/she/video/4.mp4',
+    title: 'Может я все таки могу любить?',
+    play: false,
+  },
+])
+</script>
+
+<style scoped lang="scss">
+.content {
+  max-width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.carousel-item {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column-reverse;
+  position: relative;
+
+  .image-carousel {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
