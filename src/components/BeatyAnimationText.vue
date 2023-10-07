5b839625230a3113f77934fd35f8f2a75a0f9d1f
krivalex <new.krivalex@gmail.com>
2023-10-07 20:28:29 +0600

    stage four - not designed

---
+++ src/components/BeatyAnimationText.vue
@@ -0,0 +1,65 @@
+<template>
+  <h2 v-if="show">{{ text }}</h2>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const colors = [
+  '#2196f3',
+  '#f43f5e',
+  '#ec4899',
+  '#d946ef',
+  '#a855f7',
+  '#8b5cf6',
+  '#6366f1',
+  '#3b82f6',
+  '#0ea5e9',
+  '#06b6d4',
+  '#14b8a6',
+  '#10b981',
+  '#22c55e',
+  '#84cc16',
+  '#eab308',
+  '#f59e0b',
+  '#f97316',
+  '#ef4444',
+]
+
+setInterval(() => {
+  const color = colors[Math.floor(Math.random() * colors.length)]
+  document.body.style.setProperty('--background', color)
+}, 5000)
+
+defineProps({
+  text: String,
+  show: Boolean,
+})
+</script>
+
+<style lang="scss">
+:root {
+  --color: #ffffff;
+  --background: #2196f3;
+}
+
+body {
+  font-family: 'Baloo Tamma', cursive;
+  background: var(--background);
+  color: var(--color);
+  transition: 4s;
+}
+
+h2 {
+  text-align: center;
+  width: 100vw;
+  margin: 0px auto;
+  font-size: 40px;
+  padding: 0 10px;
+  letter-spacing: 5px;
+  top: 30%;
+  position: absolute;
+  transition: 4s;
+  text-shadow: -1px -1px 0px var(--background), 3px 3px 0px var(--background), 6px 6px 0px #00000055;
+}
+</style>
--- src/sections/StageFour.vue
+++ src/sections/StageFour.vue
@@ -1,3 +1,84 @@
-<template></template>
+<template>
+  <div v-for="item in text" :key="item.letters">
+    <BeatyAnimationText v-if="item.show" :text="item.letters" :show="item.show" />
+  </div>
+  <div class="absolute">
+    <p-button v-if="nextStageApprove" label="Что же в конце?" @click="nextStage" class="next-stage-button p-button-rounded" />
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BeatyAnimationText from '../components/BeatyAnimationText.vue'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import PButton from 'primevue/button'
+
+const router = useRouter()
+
+const nextStageApprove = ref(false)
+
+const nextStage = () => {
+  router.push('/stage5')
+}
+
+onMounted(() => {
+  setTimeout(() => {
+    text.value[0].show = true
+  }, 3000)
+  setTimeout(() => {
+    text.value[0].show = false
+    text.value[1].show = true
+  }, 8000)
+  setTimeout(() => {
+    text.value[1].show = false
+    text.value[2].show = true
+  }, 13000)
+  setTimeout(() => {
+    text.value[2].show = false
+    text.value[3].show = true
+  }, 18000)
+  setTimeout(() => {
+    text.value[3].show = false
+    text.value[4].show = true
+  }, 23000)
+  setTimeout(() => {
+    text.value[4].show = false
+    nextStageApprove.value = true
+  }, 28000)
+})
+
+const text = ref([
+  {
+    letters: 'Я надеюсь тебе понравились цветы',
+    show: false,
+  },
+  {
+    letters: 'Знаешь, джентельмены не дарят цветы просто так',
+    show: false,
+  },
+  {
+    letters: 'А вот подонки вполне могут',
+    show: false,
+  },
+  {
+    letters: 'Я хочу узнать тебя получше,',
+    show: false,
+  },
+  {
+    letters: 'Попробуй понять меня, возможно все сложнее чем кажется',
+    show: false,
+  },
+])
+</script>
+
+<style lang="scss">
+.absolute {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 20px;
+}
+</style>
