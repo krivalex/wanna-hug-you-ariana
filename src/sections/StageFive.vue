580279f90074f1845a787d3215688376cfbd151e
krivalex <new.krivalex@gmail.com>
2023-10-08 22:25:43 +0600

    five stage and design upd

--- src/sections/StageFive.vue
+++ src/sections/StageFive.vue
@@ -7,21 +7,62 @@
       <source src="@/assets/bg-audio/Wet_Hands.mp3" type="audio/mp3" />
     </audio>
   </div>
-  <div class="content">
-    <h1>Я приглашаю тебя</h1>
-    <h2>На фотосет</h2>
-    <h2>И на сырный рамен</h2>
-    <p-button label="Когда?" @click="nextStage" class="next-stage-button p-button-rounded" />
+  <div class="wheel-slow-text">
+    <div v-if="firstText" class="appeariance-text text">
+      <h1>Я приглашаю тебя</h1>
+    </div>
+    <div v-if="secondText" class="appeariance-text text">
+      <h2>На фотосет</h2>
+      <img class="image" src="@/assets/photo.jpg" />
+    </div>
+    <div v-if="thirdText" class="appeariance-text text">
+      <h2>И на сырный рамен</h2>
+      <img class="image" src="@/assets/ramen.jpg" />
+    </div>
+    <p-button v-if="nextStageApprove" label="Когда?" @click="nextStage" class="next-stage-button p-button-rounded" />
   </div>
 </template>
 
 <script setup lang="ts">
 import PButton from 'primevue/button'
 import { useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
 
 const router = useRouter()
 
 const nextStage = () => {
   router.push('/stage6')
 }
+
+onMounted(() => {
+  setTimeout(() => {
+    firstText.value = true
+  }, 2000)
+  setTimeout(() => {
+    secondText.value = true
+  }, 8000)
+  setTimeout(() => {
+    thirdText.value = true
+  }, 13000)
+  setTimeout(() => {
+    nextStageApprove.value = true
+  }, 20000)
+})
+
+const nextStageApprove = ref(false)
+const firstText = ref(false)
+const secondText = ref(false)
+const thirdText = ref(false)
 </script>
+
+<style scoped lang="scss">
+.image {
+  object-fit: cover;
+  max-width: 70%;
+  max-height: 60%;
+  width: 70%;
+  height: 60%;
+  box-shadow: 0 0 3px 5px #ffffff;
+  margin-top: 15px;
+}
+</style>
